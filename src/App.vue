d892eba214e51645528530ffde90c5fc72f259ea
Dev <brownar@cokcu.com>
2024-11-17 10:12:10 -0500

    page added

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 
 <template>
   <section class="w-full h-screen flex justify-center items-center flex-col space-y-4">
-
+        <h1 class="font-smeibold">
+          Built by <a class="overline" href="https://dev-main.tech/" target="_blank">Armani D. Brown</a>
+        </h1>
   </section>
 </template>
