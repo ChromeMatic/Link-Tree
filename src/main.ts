e1bb956a7308c767f33b510efb15feb7ed078603
ChromeMatic <armanibrwn@gmail.com>
2024-11-18 10:34:43 -0500

    meta tag added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import { MotionPlugin } from '@vueuse/motion'
+import { createHead} from "@unhead/vue";
+
 import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
+const head = createHead()
 
 app.use(MotionPlugin)
+app.use(head)
 app.mount('#app')
